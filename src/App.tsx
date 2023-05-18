2d1ba673685451a600e3675596f45e592a48f13b
VladBrok <vlad.brok99@gmail.com>
2023-05-18 20:24:17 +0300

    add "MessengerScreen component skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
+import MessengerScreen from "./components/MessengerScreen/MessengerScreen";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <MessengerScreen
+        user={{
+          phone: "",
+        }}
+      />
+    </>
+  );
 }
---
+++ src/components/MessengerScreen/MessengerScreen.tsx
@@ -0,0 +1,193 @@
+import { useState } from "react";
+import "./MessengerScreen.css";
+import { IChat } from "../../types";
+import { IMessengerScreenProps } from "./MessengerScreen.types";
+
+export default function MessengerScreen(props: IMessengerScreenProps) {
+  const [chats, setChats] = useState<IChat[]>([
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+    {
+      messages: [],
+      users: [
+        {
+          phone: "",
+        },
+        {
+          phone: "79493933400",
+        },
+      ],
+    },
+  ]);
+  const [activeChatIndex, setActiveChatIndex] = useState<number | null>(null);
+
+  const getChatName = (chat: IChat): string => {
+    // TODO: format phone number
+    return (
+      chat.users.find((user) => user.phone !== props.user.phone)?.phone ||
+      "(You)"
+    );
+  };
+
+  return (
+    <div className="messenger-screen__container">
+      <div className="messenger-screen__chats-panel">
+        {chats.map((chat, i) => (
+          <div
+            className={`messenger-screen__chat-container ${
+              i === activeChatIndex
+                ? "messenger-screen__chat-container_active"
+                : ""
+            }`}
+            key={i}
+            onClick={() => setActiveChatIndex(i)}
+          >
+            <p className="messenger-screen__chat-name">{getChatName(chat)}</p>
+          </div>
+        ))}
+      </div>
+      <div className="messenger-screen__conversation-panel"></div>
+    </div>
+  );
+}
