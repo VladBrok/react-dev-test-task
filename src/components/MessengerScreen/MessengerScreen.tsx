01c13ca11445412f9911f5639beb3a74ad3af330
VladBrok <vlad.brok99@gmail.com>
2023-05-18 21:46:20 +0300

    add conversation panel header and footer

--- src/components/MessengerScreen/MessengerScreen.tsx
+++ src/components/MessengerScreen/MessengerScreen.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./MessengerScreen.css";
 import { IChat } from "../../types";
 import { IMessengerScreenProps } from "./MessengerScreen.types";
+import { assert } from "../../lib/assert";
 
 export default function MessengerScreen(props: IMessengerScreenProps) {
   const [chats, setChats] = useState<IChat[]>([
@@ -21,9 +22,6 @@ export default function MessengerScreen(props: IMessengerScreenProps) {
       users: [
         {
           phone: "",
-        },
-        {
-          phone: "79493933400",
         },
       ],
     },
@@ -161,6 +159,8 @@ export default function MessengerScreen(props: IMessengerScreenProps) {
     },
   ]);
   const [activeChatIndex, setActiveChatIndex] = useState<number | null>(null);
+  const [message, setMessage] = useState("");
+  const messageInputRef = useRef<HTMLTextAreaElement | null>(null);
 
   const getChatName = (chat: IChat): string => {
     // TODO: format phone number
@@ -170,6 +170,27 @@ export default function MessengerScreen(props: IMessengerScreenProps) {
     );
   };
 
+  const sendMessage = (): void => {
+    assert(message != "");
+
+    console.log("sending message:", message);
+
+    setMessage("");
+    messageInputRef.current?.focus();
+  };
+
+  useEffect(() => {
+    if (activeChatIndex === null) {
+      return;
+    }
+
+    assert(messageInputRef.current != null);
+
+    messageInputRef.current?.focus();
+    setMessage("");
+  }, [activeChatIndex]);
+
+  // TODO: extract some components (ConversationPanel, ...)
   return (
     <div className="messenger-screen__container">
       <div className="messenger-screen__chats-panel">
@@ -196,7 +217,45 @@ export default function MessengerScreen(props: IMessengerScreenProps) {
           </div>
         )}
         {activeChatIndex !== null && (
-          <div className="messenger-screen__conversation-panel-content"></div>
+          <div className="messenger-screen__conversation-panel-content">
+            <div className="messenger-screen__conversation-panel-header">
+              {getChatName(chats[activeChatIndex])}
+            </div>
+            <div className="messenger-screen__conversation-panel-messages"></div>
+            <div className="messenger-screen__conversation-panel-footer">
+              {/* TODO: increase rows value dynamically as user types (?)*/}
+              <textarea
+                ref={messageInputRef}
+                className="messenger-screen__message-input"
+                rows={1}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+
+              <button
+                className={`messenger-screen__send-message-button ${
+                  message ? "" : "messenger-screen__send-message-button_hidden"
+                }`}
+                onClick={sendMessage}
+              >
+                <svg
+                  viewBox="0 0 24 24"
+                  height="24"
+                  width="24"
+                  preserveAspectRatio="xMidYMid meet"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  enableBackground="new 0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M1.101,21.757L23.8,12.028L1.101,2.3l0.011,7.912l13.623,1.816L1.112,13.845 L1.101,21.757z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
         )}
       </div>
     </div>
